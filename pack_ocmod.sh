8d66fd1d9a846e2248351dff835bbc8a249eb46a
German <GermanAizek@yandex.ru>
2020-03-18 23:25:43 +0000

    Create pack_ocmod.sh

---
+++ pack_ocmod.sh
@@ -0,0 +1,4 @@
+zip -r patch.ocmod.zip ./patch/
+zip -r call_order.ocmod.zip ./modules/call_order/
+zip -r improved_breadcrumbs.ocmod.zip ./modules/improved_breadcrumbs/
+zip -r category_wall.ocmod.zip ./modules/category_wall/
